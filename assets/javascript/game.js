6fe78e78d46295d7f37fd697e8bc0776d0a50df5
Austin Robbins <jirafaro@gmail.com>
2019-06-17 23:46:47 -0700

    rough draft starting point for CSS and HTML - Complete but could use some work. Pseudo code for the Js but no js code yet.

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Define Variables 
+// Start the game with random objective score 
+    //display random objective score in score
+// Set random values for each button
+    // reset random value at start of game
+    // set onclick to call randomvalue set @ start of game
+    // add to current points block 
+    // random score for buttons is hidden
+// add win and loss tracker 
+    // win if current score = random objective score
+        // display alert you win
+            // add +1 to win 
+                //reset game
+    // else lose if current score > random objective score
+        // display alert you lost 
+            // add +1 to losses
+                //reset game
