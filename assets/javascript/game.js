f04626624db7e5e285439387af760668fce323e3
Austin Robbins <jirafaro@gmail.com>
2019-06-19 07:04:47 -0700

    variables and first function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
 // Define Variables 
+let GoalScore;
+let randomscore = [];
+let currentscore = [];
+let wins = [];
+let losses = [];
 // Start the game with random objective score 
+function randomgoal() {
+    let randomgoal = Math.floor(Math.random()*120);
+    GoalScore = randomscore[randomgoal];
+}
+randomgoal();
+console.log(randomgoal);
+// dom Manipulation 
+let docGoalScore = document.getElementById('GoalScore');
+let docwins = document.getElementById('Wins'); // Will need to seperate out Losses later 
+let doccurrentscore = document.getElementById('pointsblock');
     //display random objective score in score
 // Set random values for each button
     // reset random value at start of game
