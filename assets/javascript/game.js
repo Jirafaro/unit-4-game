9193db0f4072f7397cd9d97ebd4f66358a3b4cf5
Austin Robbins <jirafaro@gmail.com>
2019-06-19 19:32:26 -0700

    Complete and slightly polished version of the Crystal Collector Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,118 @@
 // Define Variables 
-let GoalScore;
-let randomscore = [];
-let currentscore = [];
-let wins = [];
-let losses = [];
+let GoalScore = 0;
+let gem1 = 0;
+let gem2 = 0;
+let gem3 = 0;
+let gem4 = 0;
+let currentscore = 0;
+let wins = 0;
+let losses = 0;
 // Start the game with random objective score 
+console.log(GoalScore);
 function randomgoal() {
-    let randomgoal = Math.floor(Math.random()*120);
-    GoalScore = randomscore[randomgoal];
+    GoalScore = Math.floor(Math.random()* 100)+19 ;
+    gem1 = Math.floor(Math.random()*11)+1;
+    gem2 = Math.floor(Math.random()*11)+1;
+    gem3 = Math.floor(Math.random()*11)+1;
+    gem4 = Math.floor(Math.random()*11)+1;
 }
 randomgoal();
-console.log(randomgoal);
+console.log(GoalScore);
 // dom Manipulation 
-let docGoalScore = document.getElementById('GoalScore');
-let docwins = document.getElementById('Wins'); // Will need to seperate out Losses later 
-let doccurrentscore = document.getElementById('pointsblock');
     //display random objective score in score
+    $('#GoalScore').html('Goal Score: '+ GoalScore);
 // Set random values for each button
-    // reset random value at start of game
-    // set onclick to call randomvalue set @ start of game
-    // add to current points block 
-    // random score for buttons is hidden
-// add win and loss tracker 
-    // win if current score = random objective score
-        // display alert you win
-            // add +1 to win 
-                //reset game
-    // else lose if current score > random objective score
-        // display alert you lost 
-            // add +1 to losses
-                //reset game
+$('#b1').on('click', function(){
+    // add to current points block
+    currentscore = currentscore + gem1;
+    $('#pointsblock').html(' '+ currentscore);
+    if (currentscore === GoalScore){
+        alert('You Win!');
+        wins ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses)
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    } else if(currentscore > GoalScore){
+        alert('You Lose!');
+        losses ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    }
+})
+$('#b2').on('click', function(){
+    // add to current points block
+    currentscore = currentscore + gem2;
+    $('#pointsblock').html(' '+ currentscore);
+    if (currentscore === GoalScore){
+        alert('You Win!');
+        wins ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    } else if(currentscore > GoalScore){
+        alert('You Lose!');
+        losses ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    }
+})
+$('#b3').on('click', function(){
+    // add to current points block
+    currentscore = currentscore + gem3;
+    $('#pointsblock').html(' '+ currentscore);
+    if (currentscore === GoalScore){
+        alert('You Win!');
+        wins ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    } else if(currentscore > GoalScore){
+        alert('You Lose!');
+        losses ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    }
+})
+$('#b4').on('click', function(){
+    // add to current points block
+    currentscore = currentscore + gem4;
+    $('#pointsblock').html(' '+ currentscore);
+    if (currentscore === GoalScore){
+        alert('You Win!');
+        wins ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    } else if(currentscore > GoalScore){
+        alert('You Lose!');
+        losses ++;
+        $('#Wins').html('Wins: '+ wins + '<br>'+'<hr>'+'Losses: '+ losses);
+        randomgoal();
+        currentscore = 0;
+        $('#GoalScore').html('Goal Score: '+GoalScore);
+        $('#pointsblock').html(currentscore);
+    }
+})
+
+
+
+
+
+ 
